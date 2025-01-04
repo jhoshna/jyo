44ae72366ad7bac4cbf423ebe3109b08ae908e7a
sasha <22b81a1238@cvr.ac.in>
2025-01-04 17:06:15 +0530

    msg

---
+++ prototypical.js
@@ -0,0 +1,12 @@
+function Animal(name) {
+    this.name = name;
+}
+
+Animal.prototype.speak = function () {
+    console.log(`${this.name} makes a sound.`);
+};
+
+const dog = new Animal("Bobby");
+
+console.log(dog.name);
+dog.speak();
